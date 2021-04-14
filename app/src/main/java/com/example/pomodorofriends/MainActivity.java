ae9802f36eda5aeec40d48834f276fcca64556f6
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-04-14 03:49:22 -0400

    Created bottom navigation menu

--- app/src/main/java/com/example/pomodorofriends/MainActivity.java
+++ app/src/main/java/com/example/pomodorofriends/MainActivity.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
     }
 
     @Override
