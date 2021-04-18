440e67361f0de17b472a3f5846799514648fc691
Gginne <23042760+Gginne@users.nonreply.github.com>
2021-04-18 12:59:09 -0400

    Created timer item for basket fragment

---
+++ app/src/main/java/com/example/pomodorofriends/TimerAdapter.java
@@ -0,0 +1,71 @@
+package com.example.parstagram;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.pomodorofriends.Timer;
+import com.parse.ParseFile;
+
+import java.util.List;
+
+public class TimerAdapter extends RecyclerView.Adapter<TimerAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Timer> timers;
+
+    public TimerAdapter(Context context, List<Timer> timers){
+        this.context = context;
+        this.timers = timers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_timer, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Timer timer = timers.get(position);
+        holder.bind(timer);
+    }
+
+    @Override
+    public int getItemCount() {
+        return timers.size();
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        this.timers.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Timer> posts) {
+        this.timers.addAll(posts);
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+        }
+
+        public void bind(Timer timer) {
+
+        }
+    }
+}
